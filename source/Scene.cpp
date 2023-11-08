ea24e08eab65fb5a457d93bf934e74c70e3c136d
Jakub Fratczak <jakubfratczak204@gmail.com>
2023-11-09 00:33:38 +0100

    Week 6 - Extra Scene Update
    Updated the extra scene

--- source/Scene.cpp
+++ source/Scene.cpp
@@ -280,24 +280,24 @@ SceneExtra::SceneExtra() :
 	Scene("Extra Scene", Camera(Vector3(0.0f, 3.0f, -9.0f)))
 {
 	const unsigned char
-		mirror{ AddMaterial(new CookTorrenceMaterial({ 1.0f,  1.0f,  1.0f }, 0.f, 0.0f)) },
-		smooth{ AddMaterial(new CookTorrenceMaterial({ 1.0f,  0.0f,  1.0f }, 0.f, 0.2f)) },
-		medium{ AddMaterial(new CookTorrenceMaterial({ 1.0f,  1.0f,  0.0f }, 0.f, 0.5f)) },
-		rough{ AddMaterial(new CookTorrenceMaterial({ 0.0f,  1.0f,  1.0f }, 0.f, 0.4f)) },
-		lambertWhite{ AddMaterial(new LambertMaterial(WHITE, 1.0f)) };
+		mirror{ AddMaterial(new CookTorrenceMaterial({ 1.0f,  1.0f,  1.0f }, 0.0f, 0.0f)) },
+		smootherCyan{ AddMaterial(new CookTorrenceMaterial({ 0.0f,  1.0f,  1.0f }, 0.0f, 0.1f)) },
+		smoothPink{ AddMaterial(new CookTorrenceMaterial({ 1.0f,  0.0f,  1.0f }, 0.0f, 0.2f)) },
+		roughYellow{ AddMaterial(new CookTorrenceMaterial({ 1.0f,  1.0f,  0.0f }, 0.0f, 0.4f)) };
 
-	AddPlane(Plane(Vector3(0.0f, 0.0f, 10.0f), Vector3(0.0f, 0.0f, -1.0f), mirror)); //BACK
-	AddPlane(Plane(Vector3(0.0f, 0.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), smooth)); //BOTTOM
-	AddPlane(Plane(Vector3(0.0f, 10.0f, 0.0f), Vector3(0.0f, -1.0f, 0.0f), medium)); //TOP
-	AddPlane(Plane(Vector3(5.0f, 0.0f, 0.0f), Vector3(-1.0f, 0.0f, 0.0), rough)); //RIGHT
-	AddPlane(Plane(Vector3(-5.0f, 0.0f, 0.0f), Vector3(1.0f, 0.0f, 0.0f), rough)); //LEFT
+	AddPlane(Plane(Vector3(0.0f, 0.0f, 7.0f), Vector3(0.0f, 0.0f, -1.0f), smootherCyan)); //BACK
+	AddPlane(Plane(Vector3(0.0f, 0.0f, 0.0f), Vector3(0.0f, 1.0f, 0.0f), smoothPink)); //BOTTOM
+	AddPlane(Plane(Vector3(0.0f, 10.0f, 0.0f), Vector3(0.0f, -1.0f, 0.0f), roughYellow)); //TOP
+	AddPlane(Plane(Vector3(5.0f, 0.0f, 0.0f), Vector3(-1.0f, 0.0f, 0.0), smootherCyan)); //RIGHT
+	AddPlane(Plane(Vector3(-5.0f, 0.0f, 0.0f), Vector3(1.0f, 0.0f, 0.0f), smootherCyan)); //LEFT
 
-	TriangleMesh* const pTriangleMesh{ AddTriangleMesh(TriangleMesh("Resources/lowpoly_bunny.obj", medium)) };
-	pTriangleMesh->SetScalar(2.0f);
+	TriangleMesh* const pTriangleMesh{ AddTriangleMesh(TriangleMesh("Resources/lowpoly_bunny.obj", mirror)) };
+	pTriangleMesh->SetScalar(3.0f);
+	pTriangleMesh->SetRotorY(TO_RADIANS * 45.0f);
 	pTriangleMesh->UpdateTransforms();
 	pTriangleMesh->ToggleDynamic();
 
-	AddLight(Light(Vector3(0.0f, 5.0f, 5.0f), 50.0f, ColorRGB(1.0f, 1.0f, 1.0f))); //Backlight
-	AddLight(Light(Vector3(-2.5f, 5.0f, -5.0f), 70.0f, ColorRGB(1.0f, 1.0f, 1.0f))); //Front Light Left
-	AddLight(Light(Vector3(2.5f, 2.5f, -5.0f), 50.0f, ColorRGB(1.0f, 1.0f, 1.0f)));
+	AddLight(Light(Vector3(2.5f, 2.5f, 5.0f), 70.0f, ColorRGB(1.0f, 1.0f, 1.0f)));
+	AddLight(Light(Vector3(-2.5f, 7.5f, 3.0f), 70.0f, ColorRGB(1.0f, 1.0f, 1.0f)));
+	AddLight(Light(Vector3(0.0f, 2.5f, -10.0f), 100.0f, ColorRGB(1.0f, 1.0f, 1.0f)));
 }
